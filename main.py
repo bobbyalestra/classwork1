2ba061355a63c68249ba4f8c405aa77134d6e978
Bobby Alestra <bobbyalestra135@gmail.com>
2021-10-29 10:50:28 -0400

    more class work in python

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ for number in range(1, 11):
                    break
          print (number)
 print ("Thanks!")
+
+
+for number in range(1, 11):
+         if number == 4:
+                   continue
+         print (number)
+else:
+         print ("Exited normally")
